ca284353936bbf06b965ca4e0ce53da9c71dbb96
Aviv Hasday <avivhasday@gmail.com>
2022-10-22 13:14:43 +0300

    added CRUD functionality for movie asset in MongoHandler + fixes in base class

--- utils/db_handler.py
+++ utils/db_handler.py
@@ -4,15 +4,22 @@ from typing import Union, List
 
 import pymongo
 
+from assets.movie import Movie
 
-class GenericMongoDB:
 
-    _ID_KEY_FIELD = "_id"
+class MongoFields:
+    ID_KEY_FIELD = "_id"
     INCLUDE_OPERATION = "$in"
     SET_OPERATION = "$set"
     MATCH_OPERATION = "$match"
     SORT_OPERATION = "$sort"
     LIMIT_OPERATION = "$limit"
+    ALL_EXISTS_DOCS = {"$exists": True}
+
+
+class GenericMongoDB:
+
+
 
     @staticmethod
     def is_document_already_exists(field: str, keyword_to_filter: str, collection: pymongo.collection.Collection) -> bool:
@@ -27,19 +34,19 @@ class GenericMongoDB:
     @staticmethod
     def update_document_in_collection(document_id: str, new_document_values: dict, collection: pymongo.collection.Collection) -> bool:
         res = collection.update_one(
-            {GenericMongoDB._ID_KEY_FIELD: document_id},
-            {GenericMongoDB.SET_OPERATION: new_document_values},
+            {MongoFields.ID_KEY_FIELD: document_id},
+            {MongoFields.SET_OPERATION: new_document_values},
             upsert=False
         )
         return True if res.matched_count > 0 else False
 
     @staticmethod
-    def delete_documents_from_collection(documents_ids_to_remove: [str], collection: pymongo.collection.Collection):
-        res = collection.delete_many({GenericMongoDB._ID_KEY_FIELD: {GenericMongoDB.INCLUDE_OPERATION: documents_ids_to_remove}})
-        return True if res.deleted_count == len(documents_ids_to_remove) else False
+    def delete_documents_from_collection(documents_ids_to_remove: [str], collection: pymongo.collection.Collection) -> int:
+        res = collection.delete_many({MongoFields.ID_KEY_FIELD: {MongoFields.INCLUDE_OPERATION: documents_ids_to_remove}})
+        return res.deleted_count if res else None
 
     @staticmethod
-    def query_documents_from_collection(field_to_filter: str, filter_by: Union[str, List], collection: pymongo.collection.Collection, sort_by: Union[dict, int] = 1, limit_count: int = 1000):
+    def query_documents_from_collection(field_to_filter: str, filter_by: Union[str, List, dict], collection: pymongo.collection.Collection, sort_by: Union[dict, int] = 1, limit_count: int = 1000):
         doc_list = list()
         if isinstance(filter_by, str):
             document = GenericMongoDB._query_single_document_from_collection(field_to_filter, filter_by, collection)
@@ -57,26 +64,67 @@ class GenericMongoDB:
         return document if document else None
 
     @staticmethod
-    def _query_many_documents_from_collection(field_to_filter: str, filter_by: Union[str, List], collection: pymongo.collection.Collection, sort_by: Union[dict, int], limit_count: int):
+    def _query_many_documents_from_collection(field_to_filter: str, filter_by: Union[str, List, dict], collection: pymongo.collection.Collection, sort_by: Union[dict, int], limit_count: int):
         pipeline = [
             {
-                GenericMongoDB.MATCH_OPERATION: {field_to_filter: filter_by}
+                MongoFields.MATCH_OPERATION: {field_to_filter: filter_by}
             },
             {
-                GenericMongoDB.SORT_OPERATION: sort_by
+                MongoFields.SORT_OPERATION: sort_by
             },
             {
-                GenericMongoDB.LIMIT_OPERATION: limit_count
+                MongoFields.LIMIT_OPERATION: limit_count
             }
         ]
         res = collection.aggregate(pipeline)
         return res if res else list()
 
 
-class MongoHandler:
+class MongoHandler(GenericMongoDB):
 
     def __init__(self, mongo_server_ip: str, database_name: str, port: int = 27017, logger: logging.Logger = logging.Logger):
         self.client = pymongo.MongoClient(mongo_server_ip, port)
         self.db = self.client.get_database(database_name)
         self.logger = logger
+
+    def get_filtered_list_of_documents_to_insert_to_collection(self, documents_list: [dict], collection: pymongo.collection.Collection) -> [dict]:
+        documents_names_in_db = [doc["name"] for doc in documents_list if MongoHandler.is_document_already_exists("name", doc["name"], collection)]
+        self.logger.info(f"There are {len(documents_names_in_db)}/{len(documents_list)} documents that already exists in DB, Filtering documents list to insert!")
+        filtered_docs = list(filter(lambda x: x["name"] not in documents_names_in_db, documents_list))
+        return filtered_docs
+
+
+    def get_movies(self, list_of_ids: [str] = None, list_of_names: [str] = None, sort_by: Union[dict, int] = 1, limit: int = 1000) -> [Movie]:
+        if list_of_ids is not None:
+            documents = MongoHandler.query_documents_from_collection(MongoFields.ID_KEY_FIELD, list_of_ids, self.db.movie, sort_by, limit)
+        elif list_of_names is not None:
+            documents = MongoHandler.query_documents_from_collection("name", list_of_names, self.db.movie, sort_by, limit)
+        else:
+            documents = MongoHandler.query_documents_from_collection(MongoFields.ID_KEY_FIELD, MongoFields.ALL_EXISTS_DOCS, self.db.movie, sort_by, limit)
+
+        return [Movie(**doc) for doc in documents]
+
+    def update_movie(self, movie_id: str, new_movie_details: dict):
+        res = MongoHandler.update_document_in_collection(movie_id, new_movie_details, self.db.movie)
+        if not res:
+            raise RuntimeError(f"Failed to update movie {movie_id}")
+        return True
+
+    def insert_movies(self, movies_list_to_insert: [dict]) -> [int]:
+        filtered_movie_list = self.get_filtered_list_of_documents_to_insert_to_collection(movies_list_to_insert, self.db.movie)
+        self.logger.info(f"Inserting movies to DB!")
+        inserted_ids = MongoHandler.insert_documents_to_collection(filtered_movie_list, self.db.movie)
+        if not inserted_ids:
+            raise RuntimeError("Failed to insert movies to DB")
+        if len(inserted_ids) < len(filtered_movie_list):
+            self.logger.info(f"Failed to insert all documents to DB, {len(inserted_ids)}/{len(filtered_movie_list)}")
+        return inserted_ids
+
+    def delete_movies(self, movie_ids_to_delete: [int]):
+        deleted_count = MongoHandler.delete_documents_from_collection(movie_ids_to_delete, self.db.movie)
+        if deleted_count < len(movie_ids_to_delete):
+            raise RuntimeError(f"Failed to remove all movies from db, deleted {deleted_count}/{len(movie_ids_to_delete)}")
+        self.logger.info(f"Deleted Movie IDs: {movie_ids_to_delete} from DB!")
+
+
 
