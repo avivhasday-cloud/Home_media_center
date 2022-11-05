1a6776c5ec14052cf37ca47bbb8032b6d2cf3ce1
Aviv Hasday <avivhasday@gmail.com>
2022-11-05 21:43:04 +0200

    added function to run operation on torrent (downloading,pause,resume etc) to avoid duplicated code

--- torrent_downloader.py
+++ torrent_downloader.py
@@ -8,8 +8,9 @@ from threading import Thread
 class TorrentDownloader:
     def __init__(self, server_url: str, user: str, password: str, output_dir: str, logger: logging = None):
         self.torrent_client = Client(server_url)
-        self.torrent_client.login(user, password)
         self.output_dir = output_dir
+        self.user = user
+        self.password = password
         self.logger = logger
         self.download_torrent_queue = Queue()
         self.download_torrent_thread = Thread(target=self.download_torrent_from_magnet_link)
@@ -47,14 +48,26 @@ class TorrentDownloader:
                 self.logger.info(f"Number of torrents in queue: {len(self.download_torrent_queue.queue)}")
             except Exception:
                 continue
-            try:
-                torrent_dir = os.path.join(self.output_dir, torrent_name)
-                self.torrent_client.download_from_link(torrent_dict["torrent_magnet_link"], save_path=torrent_dir)
-            except Exception as e:
-                self.logger.error(f"Failed to start download torrent {torrent_name}")
-                self.logger.error(e)
+            torrent_dir = os.path.join(self.output_dir, torrent_name)
+            os.makedirs(torrent_dir, exist_ok=True)
+            self.run_operation_on_torrent(torrent_dict['name'], "start downloading",
+                                          lambda: self.torrent_client.download_from_link(torrent_dict["torrent_magnet_link"], save_path=torrent_dir))
 
+    def resume_downloading_torrent(self, torrent: dict):
+        self.run_operation_on_torrent(torrent["name"], "resume", lambda x: self.torrent_client.resume(torrent["info_hash"]))
 
+    def pause_downloading_torrent(self, torrent: dict):
+        self.run_operation_on_torrent(torrent["name"], "pause", lambda x: self.torrent_client.pause(torrent["info_hash"]))
+
+    def run_operation_on_torrent(self, name: str, operation_name: str, func):
+        try:
+            self.torrent_client.login(self.user, self.password)
+            func()
+        except Exception as e:
+            self.logger.error(f"Failed to {operation_name} torrent {name}")
+            self.logger.error(e)
+        finally:
+            self.torrent_client.logout()
 
     def get_torrents_details(self, filter_keyword: str) -> [dict]:
         torrents = self.torrent_client.torrents(filter=filter_keyword)
